e6b8559d2c46a77cefbcfe1dbce874398fc6d347
Richard Barnes <rbarnes@umn.edu>
2020-02-28 13:49:34 -0700

    Drop utility.py

--- src/utility.py
+++
@@ -1,9 +0,0 @@
-def DropColumnsIfPresent(df, columns_to_drop):
-  """Drop columns from Pandas DataFrame regardless of whether they are there.
-
-  Dropping columns that don't exist from a Pandas DataFrame raises an exception.
-  This function avoids that.
-  """
-
-  columns_to_drop = [x for x in columns_to_drop if x in df.index]
-  return df.drop(columns=columns_to_drop)
