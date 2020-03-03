14e16dc3e05fffe3efe22bc9145afdc972c29630
Richard Barnes <rbarnes@umn.edu>
2020-03-02 20:04:00 -0700

    Ensure depots have nodes near them. Improve routing failure errors (#38)
    * Ensure depots have nodes near them. Improve routing failure errors

--- src/routingkit/routingkit.hpp
+++ src/routingkit/routingkit.hpp
@@ -14,7 +14,7 @@ class Router {
   Router(const std::string &pbf_filename);
 
   //Return the id of the nearest node to a lat,lon point
-  unsigned getNearestNode(const float lat, const float lon, const int search_radius_m);
+  unsigned getNearestNode(const double lat, const double lon, const int search_radius_m) const;
 
   //Returns <travel time (s), travel distance (m)>
   std::vector<double> getTravelTime(const double from_lat, const double from_lon, const double to_lat, const double to_lon, const int search_radius_m) const;
