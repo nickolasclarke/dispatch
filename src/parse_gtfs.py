0467454c5d4de6dc5a6259099975965133e57499
Richard Barnes <rbarnes@umn.edu>
2020-03-03 17:18:03 -0700

    Make parse_gtfs.py into a module

--- src/parse_gtfs.py
+++ src/parse_gtfs.py
@@ -300,27 +300,72 @@ def GenerateStopTimes(gtfs):
   return stop_times
 
 
-if len(sys.argv)!=3:
-  print("Syntax: {0} <GTFS File> <Output Pickle Name>".format(sys.argv[0]))
-  sys.exit(-1)
 
-feed_file   = sys.argv[1]
-output_file = sys.argv[2]
+def DoesFeedLoad(gtfs):
+  feed = ptg.load_feed(gtfs)
 
-#######################
-# Filter for service ids
 
-# find the busiest date
-date, service_ids = ptg.read_busiest_date(feed_file)
 
-print("Service id chosen = {0}".format(service_ids))
+def HasBusRoutes(gtfs):
+  #GTFS uses numeric identifiers to indicate what kind of vehicles serve a route.
+  #The relevant bus-like identifiers for us are below.
+  route_types = [
+      [3], # Standard Route Type code
 
-#Load file twice so that we don't modify it within these functions
-trips      = GenerateTrips(ptg.load_geo_feed(feed_file), date, service_ids)
-stops      = GenerateStops(ptg.load_geo_feed(feed_file))
-stop_times = GenerateStopTimes(ptg.load_geo_feed(feed_file))
-# road_segs, seg_props = GenerateRoadSegments(ptg.load_geo_feed(feed_file))
+      [700, 701, #Extended Bus codes
+      702, 703, 
+      704, 705, 
+      706, 707, 
+      708, 709, 
+      710, 711, 
+      712, 713, 
+      714, 715, 716],
 
-trips.to_csv(output_file+"_trips.csv", index=False)
-stops.to_csv(output_file+"_stops.csv", index=False)
-stop_times.to_csv(output_file+"_stop_times.csv", index=False)
+      [200, 201, #Extended Coach codes
+      202, 203, 
+      204, 205, 
+      206, 207, 
+      208, 209],
+
+      [800] #Extended Trollybus codes
+  ]
+  feed = ptg.load_feed(gtfs)
+  assert feed.routes.route_type.isin(itertools.chain(*route_types)).any()
+
+
+
+def HasBlockIDs(gtfs):
+  feed = ptg.load_feed(gtfs)
+  assert feed.trips.columns.isin(['block_id']).any()
+
+
+
+def ParseFile(gtfs_filename, output_prefix):
+  # find the busiest date
+  date, service_ids = ptg.read_busiest_date(feed_file)
+
+  print("Service id chosen = {0}".format(service_ids))
+
+  #Load file twice so that we don't modify it within these functions
+  trips      = GenerateTrips(ptg.load_geo_feed(feed_file), date, service_ids)
+  stops      = GenerateStops(ptg.load_geo_feed(feed_file))
+  stop_times = GenerateStopTimes(ptg.load_geo_feed(feed_file))
+  # road_segs, seg_props = GenerateRoadSegments(ptg.load_geo_feed(feed_file))
+
+  trips.to_csv(output_prefix+"_trips.csv", index=False)
+  stops.to_csv(output_prefix+"_stops.csv", index=False)
+  stop_times.to_csv(output_prefix+"_stop_times.csv", index=False)
+
+
+
+def main():
+  if len(sys.argv)!=3:
+    print("Syntax: {0} <GTFS File> <Output Pickle Name>".format(sys.argv[0]))
+    sys.exit(-1)
+
+  ParseFile(gtfs_filename=sys.argv[1], output_prefix=sys.argv[2])
+
+
+
+if __name__ == '__main__':
+  main()
