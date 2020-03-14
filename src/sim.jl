5dd4e83d973f448171ae44c5547ff9ffb70395a8
Richard Barnes <rbarnes@umn.edu>
2020-03-13 22:13:20 -0400

    Clean up inductive charging preprocessing

--- src/sim.jl
+++ src/sim.jl
@@ -1,5 +1,4 @@
 #!/usr/bin/env julia
-using Distributions
 using JuliaDB
 using Unitful
 
@@ -13,21 +12,29 @@ that trip.
 
 Args:
   stop_prob  - Probability of stopping at a given stop with zero duration
-  zstop_time - Seconds spent at a stop that is otherwise given zero duration
+  dp - Data pack
 """
-function GetStopChargingTime(stops, trip_id; stop_prob=0.3, zstop_time=10)
-    #Get list of stops used by this trip and merge in the stop data
-    trip_stops = filter(row->row[:trip_id]==trip_id, stops)
-    trip_stops = filter(row->row[:inductive_charging]==true, trip_stops)
-    #Total time we are scheduled to be stopped
-    stopped_time = sum(trip_stops.stop_duration)
-
-    #Some of the stops with zero duration we do end up stopping at briefly
-    zero_count = sum(trip_stops.stop_duration.==0)     #Number of zero-duration stops
-    zero_count = rand(Binomial(zero_count, stop_prob)) #Number we actually stop at
-    stopped_time += zero_count*zstop_time              #Add time spent at them
-
-    return stopped_time
+function GetInductiveChargeTimes(dp)
+    #Create a set of stops with inductive charging
+    stops = Set(select(filter(row->row[:inductive_charging]==true, dp.stops), :stop_id))
+    #Filter stop_times to those with inductive charigng
+    stop_times = filter(row->in(row[:stop_id], stops), dp.stop_times)
+    #Summarize stop time and number of stops by trip
+    stop_times = groupby(
+        (
+            stop_duration = :stop_duration=>sum,
+            stops         = :stop_duration=>x->length(x), 
+            zero_stops    = :stop_duration=>x->sum(x.==0)
+        ),
+        stops_times, #Table to groupby
+        :trip_id     #Groupby key
+    )
+    #Number we actually stop at
+    stop_times = transform(:zero_stops = dp.params.zstops_frac_stopped_at*zero_stops)
+    #How long we spend stopped, in total
+    stop_times = transform(:stop_duration => stop_duration .+ zero_stops.*dp.params.zstops_average_time)
+    stop_times = select(stop_times, (:trip_id, :stop_duration))
+    return Dict(x.trip_id=>x for x in stop_times)
 end
 
 
@@ -39,19 +46,20 @@ the road network held in router.
 Args:
     params - Parameter pack including `search_radius` - Snap stop to closest
              road network node searching within this radius.
+    dp     - A data pack
 """
-function TimeDistanceBetweenPoints(router, ll1, ll2, params)
+function TimeDistanceBetweenPoints(ll1, ll2, dp)
     try
-      time_dist = RoutingKit.getTravelTime(router, ll1[:lat], ll1[:lng], ll2[:lat], ll2[:lng], Int32(ustrip(u"m", params.search_radius)))
+      time_dist = RoutingKit.getTravelTime(dp.router, ll1.lat, ll1.lng, ll2.lat, ll2.lng, Int32(ustrip(u"m", dp.params.search_radius)))
       return (time=time_dist[1]u"s", dist=time_dist[2]u"m")
     catch e
       errmsg = sprint(showerror, e)
       if occursin("start",errmsg)
         println("No node near start position: ", ll1)
-        return (time=15u"minute", dist=5u"km")
+        return dp.params.default_td_tofrom_depot
       elseif occursin("target",errmsg)
         println("No node near target position: ", ll2)
-        return (time=15u"minute", dist=5u"km")
+        return dp.params.default_td_tofrom_depot
       else
         println(e)
         println("\nWarning: Couldn't find a node near either ",ll1," or ",ll2)
@@ -66,23 +74,20 @@ Simulate bus movement along a route. Information about the journey is stored by
 modifying "block".
 
 Args:
-    block   - Block of trips to be simulated (MODIFIED WITH OUTPUT)
-    stop_ll - Dictionary containing the latitudes and longitudes of stops
-    router  - Router object used to determine network distances between stops
-    depots  - List of depot locations
-    params  - Model parameters
+    block - Block of trips to be simulated (MODIFIED WITH OUTPUT)
+    dp    - A data pack
 """
-function RunBlock(block, stop_ll, router, depots, params)
+function RunBlock(block, dp)
     prevtrip = block[1]
 
     #Get time, distance, and energy from depot to route
-    bstart_depot = FindClosestDepotByTime(router, stop_ll[prevtrip.start_stop_id], depots, params)
-    block_energy_depot_to_start = bstart_depot[:dist] * params[:kwh_per_km]
+    bstart_depot = FindClosestDepotByTime(router, stop_ll[prevtrip.start_stop_id], dp)
+    block_energy_depot_to_start = bstart_depot[:dist] * dp.params[:kwh_per_km]
 
     #Modify first trip of block appropriately
     block[1] = merge(block[1], (;
         bus_id=1,
-        energy_left = params[:battery_cap_kwh] - block_energy_depot_to_start,
+        energy_left = dp.params[:battery_cap_kwh] - block_energy_depot_to_start,
         depot = bstart_depot[:id]
     ))
 
@@ -93,14 +98,14 @@ function RunBlock(block, stop_ll, router, depots, params)
         prevtrip = block[previ]
 
         #TODO: Incorporate charging at the beginning of trip if there is a charger present
-        trip_energy = trip.distance * params[:kwh_per_km]
+        trip_energy = trip.distance * dp.params[:kwh_per_km]
 
         #TODO: Incorporate energetics of stopping sporadically along the trip
         #trip_energy -= GetStopChargingTime(stops, trip.trip_id) #TODO: Convert to energy
 
         #Get energetics of completing this trip and then going to a depot
-        end_depot = FindClosestDepotByTime(router, stop_ll[trip.end_stop_id], depots, params)
-        energy_from_end_to_depot = end_depot[:dist] * params[:kwh_per_km]
+        end_depot = FindClosestDepotByTime(router, stop_ll[trip.end_stop_id], dp)
+        energy_from_end_to_depot = end_depot[:dist] * dp.params[:kwh_per_km]
 
         #Energy left to perform this trip
         energy_left_this_trip = prevtrip.energy_left
@@ -110,8 +115,8 @@ function RunBlock(block, stop_ll, router, depots, params)
             #We can't complete this trip and get back to the depot, so it was better to end the block after the previous trip
             #TODO: Assert previous trip ending stop_id is same as this trip's starting stop_id
             #Get closest depot and energetics to the start of this trip (which is also the end of the previous trip)
-            start_depot = FindClosestDepotByTime(router, stop_ll[trip.start_stop_id], depots, params)
-            energy_from_start_to_depot = start_depot[:dist] * params[:kwh_per_km]
+            start_depot = FindClosestDepotByTime(router, stop_ll[trip.start_stop_id], dp)
+            energy_from_start_to_depot = start_depot[:dist] * dp.params[:kwh_per_km]
 
             #TODO: Something bad has happened if we've reached this: we shouldn't have even made the last trip.
             if energy_from_start_to_depot < prevtrip.energy_left
@@ -120,7 +125,7 @@ function RunBlock(block, stop_ll, router, depots, params)
 
             #Alter the previous trip to note that we ended it
             energy_left_last_trip = prevtrip.energy_left-energy_from_start_to_depot
-            charge_time = (params[:battery_cap_kwh]-energy_left_last_trip)/params[:charging_rate]
+            charge_time = (dp.params[:battery_cap_kwh]-energy_left_last_trip)/dp.params[:charging_rate]
             block[previ] = merge(block[previ], (;
                 energy_left = energy_left_last_trip,
                 bus_busy_end = prevtrip.bus_busy_end+start_depot[:time] + charge_time,
@@ -128,7 +133,7 @@ function RunBlock(block, stop_ll, router, depots, params)
             ))
 
             #TODO: Assumes that trip from trip start to depot and from depot to trip start is the same length
-            energy_left_this_trip = params[:battery_cap_kwh]-energy_from_start_to_depot
+            energy_left_this_trip = dp.params[:battery_cap_kwh]-energy_from_start_to_depot
 
             #Alter this trip so that we start it with a fresh bus
             block[tripi] = merge(block[tripi], (;
@@ -154,8 +159,8 @@ function RunBlock(block, stop_ll, router, depots, params)
     end
 
     #Get energetics of getting from the final trip to its depot
-    bend_depot = FindClosestDepotByTime(router, stop_ll[prevtrip.start_stop_id], depots, params)
-    block_energy_end_to_depot = bend_depot[:dist] * params[:kwh_per_km]
+    bend_depot = FindClosestDepotByTime(router, stop_ll[prevtrip.start_stop_id], dp)
+    block_energy_end_to_depot = bend_depot[:dist] * dp.params[:kwh_per_km]
 
     #Adjust beginning and end
     block[1]   = merge(block[1], (;bus_busy_start = block[1].bus_busy_start - bstart_depot[:time]))
@@ -170,13 +175,10 @@ Simulate a set of trips to determine which buses are needed when and how much
 energy is used.
 
 Args:
-    trips   - List of trips to be taken
-    stop_ll - Dictionary containing the latitudes and longitudes of stops
-    router  - Router object used to determine network distances between stops
-    depots  - List of depot locations
-    params  - Model parameters
+    trips     - List of trips to be taken
+    data_pack - A data pack
 """
-function Model(trips, stop_ll, router, depots, params)
+function Model(trips, data_pack)
     trips = sort(trips, :start_arrival_time)
 
     #Add/zero out the bus_id column
@@ -186,10 +188,13 @@ function Model(trips, stop_ll, router, depots, params)
     trips = transform(trips, :bus_busy_end   => -1.0u"s"    *ones(Float64, length(trips)))
     trips = transform(trips, :depot          => -1          *ones(Int64,   length(trips)))
 
+    #Add the inductive charging time to the data pack
+    data_pack = (data_pack..., inductive_charge_time = GetInductiveChargeTimes(data_pack))
+
     bus_swaps = []
     for (block_id,block) in groupby(identity, trips, :block_id)
         print(".")
-        RunBlock(block, stop_ll, router, depots, params)
+        RunBlock(block, data_pack)
     end
     println("done")
 
@@ -206,20 +211,19 @@ let                                #Local scope for memoization
     Given a set of depots, find the closest one to a query point.
 
     Args:
-        router   - Router object used to determine network distances between stops
         query_ll - LatLng point to find closest depot to
-        depots   - List of depot locations
+        dp       - A data pack
 
     Returns:
         (seconds to nearest depot, meters to nearest depot, id of nearest depot)
     """
-    function FindClosestDepotByTime(router, query_ll, depots, params)
+    function FindClosestDepotByTime(query_ll, dp)
         if haskey(cached, query_ll) #Return cached answer, if we have one
             return cached[query_ll]
         end
         mintime = (time=Inf*u"s",dist=Inf*u"m",id=0) #Infinitely bad choice of depot
         for i in 1:length(depots)                    #Search all depots for a better one
-            timedist = TimeDistanceBetweenPoints(router, query_ll, depots[i], params)
+            timedist = TimeDistanceBetweenPoints(query_ll, depots[i], dp)
             if timedist[:time]<mintime[:time]
                 mintime = (timedist..., id=i)
             end
@@ -278,10 +282,13 @@ stop_times = transform(stop_times, :stop_duration => :stop_duration => x->x*u"s"
 
 #TODO: Get these from command-line args or a config file
 params = (
-  battery_cap_kwh = 200.0u"kW*hr",
-  kwh_per_km      = 1.2u"kW*hr/km",
-  charging_rate   = 150.0u"kW",
-  search_radius   = 1.0u"km"
+  battery_cap_kwh         = 200.0u"kW*hr",
+  kwh_per_km              = 1.2u"kW*hr/km",
+  charging_rate           = 150.0u"kW",
+  search_radius           = 1.0u"km",
+  zstops_frac_stopped_at  = 0.2,
+  zstops_average_time     = 10u"s",
+  default_td_tofrom_depot = (time=15u"minute", dist=5u"km")
 )
 
 #Convert stops table into dictionary for fast lookups.
@@ -293,5 +300,14 @@ if ~DepotsHaveNodes(router, depots, params)
     throw("One or more of the depots don't have road network nodes! Quitting.")
 end
 
+data_pack = (
+    stops      = stops,      
+    stop_times = stop_times,
+    router     = router,     #Router object used to determine network distances between stops
+    stop_ll    = stop_ll,    #Dictionary containing the latitudes and longitudes of stops
+    depots     = depots,     #List of depot locations
+    params     = params      #Model parameters
+)
+
 #Run the model
-bus_assignments = Model(trips, stop_ll, router, depots, params);
+bus_assignments = Model(trips, data_pack);
