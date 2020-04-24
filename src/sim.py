3f616d6ff81f56ce65b910f05ebf9015f5025855
Richard Barnes <rbarnes@umn.edu>
2020-04-23 19:40:40 -0600

    Drop inductive charging function

--- src/sim.py
+++ src/sim.py
@@ -23,40 +23,6 @@ def ConvertVectorOfStructsToDataFrame(vos):
 
 
 
-def GetInductiveChargeTimes(dp):
-  """
-  For a given trip_id, determine how much time is spent waiting at stops during
-  that trip.
-
-  Args:
-    stop_prob  - Probability of stopping at a given stop with zero duration
-    dp - Data pack
-  """
-  pass
-    #Create a set of stops with inductive charging
-      # stops = Set(select(filter(row->row[:inductive_charging]==true, dp.stops), :stop_id))
-    #Filter stop_times to those with inductive charigng
-      # stop_times = filter(row->in(row[:stop_id], stops), dp.stop_times)
-    #Summarize stop time and number of stops by trip
-        # stop_times = groupby(
-        #     (
-        #         stop_duration = :stop_duration=>sum,
-        #         stops         = :stop_duration=>x->length(x),
-        #         zero_stops    = :stop_duration=>x->sum(x.==0)
-        #     ),
-        #     stop_times,  #Table to groupby
-        #     :trip_id     #Groupby key
-        # )
-    #Number we actually stop at
-      # stop_times = transform(stop_times, :zero_stops => dp.params.zstops_frac_stopped_at*select(stop_times, :zero_stops))
-    #How long we spend stopped, in total
-      #stop_times = transform(stop_times, :stop_duration => select(stop_times, :stop_duration) .+ select(stop_times, :zero_stops).*dp.params.zstops_average_time)
-      # stop_times = select(stop_times, (:trip_id, :stop_duration))
-      # return Dict(x.trip_id=>x for x in stop_times)
-
-
-
-
 def GetNearestDepots(router, trips, stops, depots, search_radius_m=1000):
   stops = stops.copy()
   #Get set of stops that are actually at the end of trips
