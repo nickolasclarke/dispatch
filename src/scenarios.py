18dcac69c529d0d38c4d858bfe2524bd3d3168b8
Nick Clarke <nickolasclarke@gmail.com>
2020-04-30 00:08:37 -0700

    refactor scenarios.py

--- src/scenarios.py
+++ src/scenarios.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import yaml
 
 import numpy as np
 from itertools import product
@@ -7,7 +8,7 @@ from itertools import product
 from sim import simulate as sim
 
 #TODO add charger_density once supported in sim.py
-def genScenarios(battery_cap_kwh,nondepot_charger_rate,): #charger_density):
+def genScenarios(battery_cap_kwh,nondepot_charger_rate,):
     '''Generates a list of tuples of scenarios
         args: all tuples of a start (inclusive), end (exclusive), and step 
         size values. i.e. battery_cap_kwh=(125,700,100) 
@@ -15,10 +16,10 @@ def genScenarios(battery_cap_kwh,nondepot_charger_rate,): #charger_density):
     #TODO include error handling
     bat_range = np.arange(*battery_cap_kwh)
     non_depot_range = np.arange(*nondepot_charger_rate)
-    #charger_density = np.arange(*charger_density)
-
-    scen_opts = [bat_range,non_depot_range,]# charger_density]
+    scen_opts = [bat_range,non_depot_range,]
     scens = list(product(*scen_opts))
+    keys = list(range(len(scens)))
+    scens = dict(zip(keys, scens))
     return scens
 
 def main():
@@ -26,53 +27,57 @@ def main():
   parser.add_argument('parsed_gtfs_prefix', type=str, help='TODO')
   parser.add_argument('osm_data',           type=str, help='TODO')
   parser.add_argument('depots_filename',    type=str, help='TODO')
-  parser.add_argument('output_filename',    type=str, help='TODO')
+  parser.add_argument('output_dir',         type=str, help='TODO')
+  parser.add_argument('--sim-parameters',   type=str, help='TODO')
   parser.add_argument('-b','--battery-cap-kwh',      nargs=3,type=int,help='TODO')
   parser.add_argument('-c','--nondepot-charger-rate',nargs=3,type=int,help='TODO')
-  #TODO add when supported by sim.py
-  #parser.add_argument('-d','--charger-density',  nargs=3,type=int,help='TODO')
-  parser.add_argument('-s','--save_csv',
-    nargs='?',const='scenarios',type=str,help='TODO')
+  #TODO add charger_density when supported by sim.py
+  # parser.add_argument('-s','--save_csv',nargs='?',const='scenarios_results',type=str,help='TODO')
   args = parser.parse_args()
 
   #extract scenario-related arguments and generate scenario array
-  list_args = set(['battery_cap_kwh','nondepot_charger_rate',])#'charger_density'])
+  list_args = set(['battery_cap_kwh','nondepot_charger_rate',])
   scen_args = {k:v for (k,v) in vars(args).items() if k in list_args}
   scenarios = genScenarios(**scen_args)
+  scen_costs = []
+  params = {}
+  if args.sim_parameters is not None:
+    with open(args.sim_parameters, 'r') as f:
+      params = yaml.full_load(f)
 
-  if args.save_csv is not None:
-    #TODO sync up with output_filename
-    output_name = f'{args.save_csv}_scenarios.csv'
-    np.savetxt(output_name,scenarios, header='battery_cap_kwh,nondepot_charger_rate',
-      delimiter=',',comments='',fmt='%1.2f')
+  for key,val in scenarios.items():
+    params['battery_cap_kwh'],params['nondepot_charger_rate'] = val[0], val[1]
+    results = sim(args.parsed_gtfs_prefix, 
+                  args.osm_data,
+                  args.depots_filename,
+                  parameters=params)
+    # parse results and write to file
+    scen_costs.append([val[0],val[1],
+      results['opti_buses'],results['opti_chargers'],results['opti_cost'],
+      results['nc_buses'],results['nc_chargers'],results['nc_cost'],
+      results['ac_buses'],results['ac_chargers'],results['ac_cost']]
+      )
 
-  for scen in scenarios:
-    print(scen)
-    params = {'battery_cap_kwh':scen[0], 'nondepot_charger_rate':scen[1]}
-    print(params)
+    prefix = f'{val[0]}kwh_{val[1]}_kw'
+    results['opti_trips'].to_csv(f'{args.output_dir}/{prefix}_trips.csv')
 
-    bus_assignments_df, depot_counts = sim(args.parsed_gtfs_prefix, 
-                                           args.osm_data,
-                                           args.depots_filename,
-                                           args.output_filename,
-                                           parameters=params)
-    # write results to file
-    print(bus_assignments_df.head(), depot_counts)
-    bus_assignments_df.to_csv(
-      f'{args.output_filename}_{scen[0]}kwh_{scen[1]}kw.csv')
-    
-    print(depot_counts)
-    depot_counts = {f'depot_{k}': v for k, v in depot_counts.items()}
-    depot_res_name = f'{args.output_filename}_depot_counts.csv'
-
-    with open(depot_res_name, 'w', newline='') as csvfile:
-      fieldnames = list(depot_counts.keys())
-      writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    depot_res_name = f'{args.output_dir}/{prefix}_depot_counts.csv'
+    with open(depot_res_name,'w',newline='') as csvfile:
+      fieldnames = ['depot','bus_count']
+      writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
       writer.writeheader()
-      for key, val in depot_counts.items():
-        writer.writerow({key: val})
+      #TODO include nc and ac depots as well
+      for key, val in results['opti_depot_counts'].items():
+        writer.writerow({'depot': key, 'bus_count': val})
 
-  return bus_assignments_df, depot_counts
+  #TODO results in cleaner way to do this?
+  results_headers = 'battery_cap_kwh,nondepot_charger_rate,\
+optimized_buses,optimized_chargers,optimized_cost,\
+nc_buses,nc_chargers,nc_cost,\
+ac_buses,ac_chargers,ac_costs'
+
+  np.savetxt(f'{args.output_dir}/scenarios_results.csv',scen_costs,
+    header=results_headers,delimiter=',',comments='',fmt='%1.2f')
 
 if __name__ == '__main__':
   main()
